d57a38e03a7a7601022258b3e52585733880e2f4
Alexandru Rusovici <alex.rusovici@yahoo.com>
2023-02-22 19:47:01 +0200

    environments
    environments

--- src/api/users.js
+++ src/api/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://plastic-elite-bayberry.glitch.me/';
+const baseUrl = process.env.REACT_APP_SERVER_URI;
 
 export const client = axios.create({
   baseURL: baseUrl,
